b65901d25cd81931beda5d3aea1e4c8a9517b3d1
pyven-dr <pyven-dr@student.42lyon.fr>
2024-07-31 00:16:32 +0200

    change the calculation to get the angle to fix the distortion on walls

--- src/render/render_frame.c
+++ src/render/render_frame.c
@@ -19,29 +19,24 @@ static void	fix_fisheye(t_inter	*wall_dist, double angle, double player_angle)
 {
 	double	angle_fix;
 
-	angle_fix = angle - player_angle;
-	angle_fix = normalize_angle(angle_fix);
+	angle_fix = normalize_angle(angle - player_angle);
 	wall_dist->distance *= cos(angle_fix);
 }
 
 int	render_frame(t_map *map, t_player *player, t_mlx *mlx_data)
 {
 	double	angle;
-	double	angle_inc;
 	t_inter	closest_wall;
 	int		i;
 
 	i = 0;
-	angle_inc = player->fov / PLANE_WIDTH;
-	angle = normalize_angle(player->angle - (player->fov / 2));
-
 	while (i < PLANE_WIDTH)
 	{
+		angle = normalize_angle(atan2((i - 0.5) - PLANE_WIDTH / 2.0, player->plane_dist) + player->angle);
 		closest_wall = find_closest_wall(angle, player, map);
 		fix_fisheye(&closest_wall, angle, player->angle);
 		trace_column(closest_wall, i, mlx_data, player->plane_dist);
 		i++;
-		angle = normalize_angle(angle + angle_inc);
 	}
 	return (0);
 }
