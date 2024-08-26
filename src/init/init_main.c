cc90d808a20fec20ac3f6e2c746afb2795c92be3
tcoze <tcoze@student.42lyon.fr>
2024-08-26 03:59:59 +0200

    EIWFJHWFEFJWFGEFW

--- src/init/init_main.c
+++ src/init/init_main.c
@@ -6,7 +6,7 @@
 /*   By: tcoze <tcoze@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 00:30:35 by tcoze             #+#    #+#             */
-/*   Updated: 2024/08/26 01:04:04 by tcoze            ###   ########.fr       */
+/*   Updated: 2024/08/26 02:37:55 by tcoze            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,18 @@ void	init_game_data(t_game_data *game_data)
 {
 	game_data->p.keys.backward = 0;
 	game_data->p.keys.forward = 0;
+	game_data->p.keys.right = 0;
+	game_data->p.keys.left = 0;
 	game_data->p.keys.look_left = 0;
 	game_data->p.keys.look_right = 0;
 	game_data->p.keys.esc = 0;
 	game_data->p.keys.up = 0;
 	game_data->p.keys.down = 0;
 	game_data->p.keys.alt = 0;
+	game_data->p.keys.crouch = 0;
 	game_data->p.vert_offset = 1;
 	game_data->p.player_size = 1;
+	game_data->p.keys.fly = 0;
 	game_data->p.keys.mouse.is_recentering = 0;
 	game_data->p.keys.mouse.x = WIDTH / 2;
 	game_data->p.keys.mouse.y = HEIGHT / 2;
