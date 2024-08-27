a1aea3bfef34bc74c7a0edee5c560073dc785c95
pyven-dr <pyven-dr@student.42lyon.fr>
2024-08-27 23:12:08 +0200

    norm

--- src/init/init_main.c
+++ src/init/init_main.c
@@ -14,38 +14,53 @@
 #include "const_values.h"
 #include <stddef.h>
 
+static void	init_keys(t_keys *keys)
+{
+	keys->backward = 0;
+	keys->forward = 0;
+	keys->look_left = 0;
+	keys->look_right = 0;
+	keys->esc = 0;
+	keys->up = 0;
+	keys->down = 0;
+	keys->left = 0;
+	keys->right = 0;
+	keys->alt = 0;
+	keys->hood = 0;
+	keys->menu = 1;
+	keys->fly = 0;
+	keys->crouch = 0;
+	keys->map = 0;
+	keys->object = 0;
+	keys->object_sec = 0;
+	keys->mouse.is_recentering = 0;
+	keys->mouse.x = WIDTH / 2;
+	keys->mouse.y = HEIGHT / 2;
+}
+
+static void	init_map_data(t_map_data *map_data)
+{
+	map_data->object.frame_count = 0;
+	map_data->object.current_img = 1;
+	map_data->north.path = NULL;
+	map_data->east.path = NULL;
+	map_data->west.path = NULL;
+	map_data->south.path = NULL;
+	map_data->north.img = NULL;
+	map_data->south.img = NULL;
+	map_data->east.img = NULL;
+	map_data->west.img = NULL;
+}
+
+static void	init_player(t_player *player)
+{
+	player->vert_offset = 1;
+	player->player_size = 1;
+}
+
 void	init_game_data(t_game_data *game_data)
 {
-	game_data->p.keys.backward = 0;
-	game_data->p.keys.forward = 0;
-	game_data->p.keys.look_left = 0;
-	game_data->p.keys.look_right = 0;
-	game_data->p.keys.esc = 0;
-	game_data->p.keys.up = 0;
-	game_data->p.keys.down = 0;
-	game_data->p.keys.left = 0;
-	game_data->p.keys.right = 0;
-	game_data->p.keys.alt = 0;
-	game_data->p.vert_offset = 1;
-	game_data->p.player_size = 1;
-	game_data->p.keys.hood = 0;
-	game_data->p.keys.menu = 1;
-	game_data->p.keys.fly = 0;
-	game_data->p.keys.crouch = 0;
-	game_data->p.keys.map = 0;
-	game_data->p.keys.object = 0;
-	game_data->p.keys.object_sec = 0;
-	game_data->map_data.object.frame_count = 0;
-	game_data->map_data.object.current_img = 1;
-	game_data->p.keys.mouse.is_recentering = 0;
-	game_data->p.keys.mouse.x = WIDTH / 2;
-	game_data->p.keys.mouse.y = HEIGHT / 2;
-	game_data->map_data.north.path = NULL;
-	game_data->map_data.east.path = NULL;
-	game_data->map_data.west.path = NULL;
-	game_data->map_data.south.path = NULL;
-	game_data->map_data.north.img = NULL;
-	game_data->map_data.south.img = NULL;
-	game_data->map_data.east.img = NULL;
-	game_data->map_data.west.img = NULL;
+	init_keys(&game_data->p.keys);
+	init_map_data(&game_data->map_data);
+	init_player(&game_data->p);
 }
